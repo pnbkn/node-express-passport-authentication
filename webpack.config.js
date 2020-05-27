51817fb4c05e9cd790bbb15e5c82bad13cf6754a
pnbkn <pnblackburn@gmail.com>
2020-05-27 16:04:07 -0700

    Set up material UI form, fixed webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist')
+  },
     module: {
       rules: [
         {
           test: /\.js$/,
           loader: 'babel-loader',
           exclude: /node_modules/
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
         }
       ]
     }
